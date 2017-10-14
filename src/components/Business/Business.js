893a70a20af55389eb327356d8d27ca270356c17
asterix135 <chgraham@gmail.com>
2017-10-14 19:24:52 -0400

    first stage of project complete

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -31,12 +31,12 @@ class Business extends React.Component {
           <div className="Business-reviews">
             <h3>{business.category}</h3>
             <h3 className="rating">{business.rating} stars</h3>
-            <p>{busines.reviewCount} reviews</p>
+            <p>{business.reviewCount} reviews</p>
           </div>
         </div>
       </div>
-    )
-  }
+    );
+  };
 };
 
 export default Business;
